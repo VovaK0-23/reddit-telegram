a454ca37c2001db5c4e5535322e78c0fc52f2335
Vova <451585@gmail.com>
2020-12-16 23:01:18 +0100

    users_controller sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  #validates :username, presence: true,
-  #uniqueness: { case_sensitive: false },
-  #length: { minimum: 3, maximum: 25 }
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true,
-                    uniqueness: { case_sensitive: false },
-                    length: { maximum: 105 },
-                    format: { with: VALID_EMAIL_REGEX }
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
   enum role: %i[standard premium admin]
 
