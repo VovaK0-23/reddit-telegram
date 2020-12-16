102c4df97efcafa51f7fc5e1a7f9672a54bd76fe
Vova <451585@gmail.com>
2020-12-16 19:45:08 +0100

    user sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new'
+  resource :users, except: [:new]
 end
