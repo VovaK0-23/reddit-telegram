7f717e5fe58280da49eaf7c649eaa8599bbd8b52
Vova <451585@gmail.com>
2020-12-17 23:33:35 +0100

    fixup! add posts

--- app/views/posts/_post.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! post, :id, :title, :body, :published_at, :user_id, :created_at, :updated_at
-json.url post_url(post, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
