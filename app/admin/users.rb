f1ca39bd5f938e496a41e166b41551d052864981
Vova <451585@gmail.com>
2020-12-21 12:39:49 +0100

    fix user roles edit

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -21,14 +21,22 @@ ActiveAdmin.register User do
   filter :role
 
   form do |f|
-    f.inputs do
-      f.input :username
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
-      f.input :role
+    if current_user.admin?
+      f.inputs do
+        f.input :username
+        f.input :email
+        f.input :password
+        f.input :password_confirmation
+        f.input :role
+      end
+    else
+      f.inputs do
+        f.input :username
+        f.input :email
+        f.input :password
+        f.input :password_confirmation
+      end
+      f.actions
     end
-    f.actions
   end
-
 end
