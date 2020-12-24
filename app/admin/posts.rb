437da0a0e9ce01cb1ca0550c0370c23bc23c3b32
Vova <451585@gmail.com>
2020-12-24 01:56:05 +0100

    publish posts in tg

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -6,22 +6,25 @@ ActiveAdmin.register Post do
   scope :unpublished
 
   action_item :publish, only: :show do
-    link_to "Publish", publish_admin_post_path(post), method: :put unless post.published_at?
+    link_to 'Publish', publish_admin_post_path(post), method: :put unless post.published_at?
   end
 
   action_item :publish, only: :show do
-    link_to "Unpublish", unpublish_admin_post_path(post), method: :put if post.published_at?
+    link_to 'Send again', publish_admin_post_path(post), method: :put if post.published_at?
   end
 
   member_action :publish, method: :put do
     post = Post.find(params[:id])
     post.update(published_at: Time.zone.now)
+    chat_id = Chat.find(post.chat_id).name
+    message = TelegramService::TelegramClient.new(chat_id)
+    message.send_message(post.body)
     redirect_to admin_post_path(post)
   end
 
-  member_action :unpublish, method: :put do
-    post = Post.find(params[:id])
-    post.update(published_at: nil)
-    redirect_to admin_post_path(post)
-  end
+  # member_action :unpublish, method: :put do
+  #   post = Post.find(params[:id])
+  #   post.update(published_at: nil)
+  #   redirect_to admin_post_path(post)
+  # end
 end
