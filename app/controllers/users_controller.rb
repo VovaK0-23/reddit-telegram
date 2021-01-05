a32fe4eef2e987cfdaa31efeed51fce119ee11f4
Vova <451585@gmail.com>
2021-01-05 22:35:00 +0100

    add edit to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,32 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'Your account was successfully updated'
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome #{@user.username}, have successfully sign up"
-      redirect_to users_path
+      redirect_to root_path
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
   get 'signup', to: 'users#new'
-  resource :users, except: %i[new]
-  root to: 'users#new'
+  resources :users, except: %i[new]
 end
