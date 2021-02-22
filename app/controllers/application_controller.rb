78a083c0ecef6d1cacc8bea1f974c7b7ca02b4c3
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2021-02-22 21:30:47 +0100

    I18n settings and button to switch language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = current_user.try(:locale) || I18n.default_locale
+  end
 
 end
