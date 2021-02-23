311d054f6411354459ca5220f9c88a3aee6cb120
LeonidFilbert <leonidfilbert@gmail.com>
2021-02-23 10:42:35 +0200

    Fix locales
    fix locales for nor signed users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    I18n.locale = current_user.try(:locale) || params[:locale] || I18n.default_locale
+    if user_signed_in? && params[:locale].present? && current_user.locale != params[:locale]
+      current_user.update(locale: params[:locale])
+    end
+
+    session[:locale] = current_user.try(:locale) || params[:locale] || I18n.default_locale
+    I18n.locale = session[:locale]
   end
 end
