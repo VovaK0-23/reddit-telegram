d35914abcb24e7bd4f5340421accdf6c3556a558
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2021-03-02 03:05:33 +0100

    front end
    1

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -6,22 +6,38 @@ $(document).ready(function() {
     $("#error-container").fadeOut(6000);
     $("#notice-container").fadeOut(6000);
 
-    let win = $(this); //this = window
-    let drpdwn = $(".q");
-    if (win.width() >= 720) {
-        drpdwn.hide();}
-    else if (win.width() < 720){
-        drpdwn.show();
+
+    if ($(".chat-index").length) {
+        // timer that reloads page when url changes
+        let myVar = setInterval(urlChange, 1000);
+        let url = window.location.href;
+
+        function urlChange() {
+            if (window.location.href !== url) {
+                clearInterval(myVar);
+                location.reload();
+            }
+        }
+
+        let win = $(this); //this = window
+        let drpdwn = $(".q");
+        if (win.width() >= 720) {
+            drpdwn.hide();
+        } else if (win.width() < 720) {
+            drpdwn.show();
+        }
+
+        $(window).on('resize', function () {
+            let win = $(this); //this = window
+            let drpdwn = $(".q");
+            if (win.width() >= 720) {
+                drpdwn.hide();
+            } else {
+                drpdwn.show();
+            }
+        });
     }
 });
 
-$(window).on('resize', function() {
-    let win = $(this); //this = window
-    let drpdwn = $(".q");
-    if (win.width() >= 720) {
-        drpdwn.hide();}
-    else {
-        drpdwn.show();
-    }
-});
+
 
